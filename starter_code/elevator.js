ee6983736502181854edd7188fa454ca91d917a3
Torgeir Beyer <torgeir.beyer@gmail.com>
2017-11-06 22:44:47 +0100

    done with a couple of iterations

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,5 +1,4 @@
 "use strict";
-const Person = require("./person.js");
 
 class Elevator {
   constructor() {
@@ -8,39 +7,66 @@ class Elevator {
     this.direction = "";
 
     this.requests = [];
+    this.passengers = [];
+    this.waitingList = [];
     this._startId = null;
   }
 
   start() {
     this._startId = setInterval(() => {
-      this.update;
+      this.update();
     }, 1000);
   }
 
   stop() {
-    clearInterval(this.startId);
+    setTimeout(() => {
+      if (this.requests === this.floor) {
+        clearInterval(this.startId);
+      }
+    });
   }
+
   update() {
     this.log();
   }
-  _passengersEnter() {}
-  _passengersLeave() {}
+
+  _passengersEnter(passenger) {
+    if (this.floor === this.requests) {
+      this.stop();
+      this.passengers.push(passenger.name);
+      this.waitingList.shift();
+      this.requests.push(passenger.destinationFloor);
+    }
+    console.log(
+      `${passenger.name} entered the elevator at floor ${passenger.originFloor}`
+    );
+  }
+
+  _passengersLeave(passenger) {
+    if (passenger.destinationFloor === this.floor) {
+      this.stop();
+      this.passengers.shift();
+    }
+    console.log(`${person.name} has left the elevator`);
+  }
+
   floorUp() {
     if (this.floor < this.MAXFLOOR) {
       this.floor += 1;
       this.direction = "Up";
     }
   }
+
   floorDown() {
     if (this.floor > 0) {
       this.floor -= 1;
       this.direction = "Down";
     }
   }
-  call() {
-    let person = new Person();
-    this.requests.push(person);
-    console.log(this.requests);
+
+  call(passenger) {
+    this.waitingList.push(passenger);
+    this.requests.push(passenger.originFloor);
   }
   log() {
     console.log(`Direction: ${this.direction}`, `Floor: ${this.floor}`);
--- starter_code/index.js
+++ starter_code/index.js
@@ -3,7 +3,9 @@ const Elevator = require("./elevator.js");
 const Person = require("./person.js");
 
 const elevator = new Elevator();
+let passenger = new Person("Torgeir", 1, 5);
 
 elevator.start();
 elevator.floorUp();
-elevator.call();
+elevator.call(passenger);
+elevator._passengersEnter(passenger);
